3317ac086c8b275cf2b3af0a36ed1b43ffb11a93
ArtificialGorillas <rodericgickle@gmail.com>
2024-09-13 18:40:55 -0700

    Update Fly.cs

--- Mods/Fly.cs
+++ Mods/Fly.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using TheGorillaWatch.Models;
@@ -8,11 +8,11 @@ namespace TheGorillaWatch.Mods
 {
     class Fly : Page
     {
-        public override string modName => "Fly";
+        public override string modName => "VelocityFly";
 
         public override void OnUpdate()
         {
-            if (ControllerInputPoller.instance.leftControllerPrimaryButton)
+            if (ControllerInputPoller.instance.rightControllerPrimaryButton)
             {
                 GorillaLocomotion.Player.Instance.GetComponent<Rigidbody>().velocity = GorillaLocomotion.Player.Instance.headCollider.transform.forward * Time.deltaTime * 1400f;
             }
