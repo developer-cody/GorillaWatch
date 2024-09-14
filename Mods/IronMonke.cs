9ad5c460ffe4b043c863ac9c6f0bc6e0e63593e4
ArtificialGorillas <rodericgickle@gmail.com>
2024-09-13 18:41:45 -0700

    Update IronMonke.cs

--- Mods/IronMonke.cs
+++ Mods/IronMonke.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using TheGorillaWatch.Models;
@@ -12,14 +12,14 @@ namespace TheGorillaWatch.Mods
 
         public override void OnUpdate()
         {
-            if (ControllerInputPoller.instance.leftGrab)
+            if (ControllerInputPoller.instance.leftControllerIndexFloat > .5f)
             {
                 GorillaLocomotion.Player.Instance.bodyCollider.attachedRigidbody.AddForce(10 * GorillaTagger.Instance.offlineVRRig.transform.Find("rig/body/shoulder.L/upper_arm.L/forearm.L/hand.L").right, ForceMode.Acceleration);
                 GorillaTagger.Instance.StartVibration(true, GorillaTagger.Instance.tapHapticStrength / 50f * GorillaLocomotion.Player.Instance.bodyCollider.attachedRigidbody.velocity.magnitude, GorillaTagger.Instance.tapHapticDuration);
             }
-            if (ControllerInputPoller.instance.rightGrab)
+            if (ControllerInputPoller.instance.rightControllerIndexFloat > .5f)
             {
-                GorillaLocomotion.Player.Instance.bodyCollider.attachedRigidbody.AddForce(10 * -GorillaTagger.Instance.offlineVRRig.transform.Find("rig/body/shoulder.R/upper_arm.R/forearm.R/hand.R").right, ForceMode.Acceleration);
+                GorillaLocomotion.Player.Instance.bodyCollider.attachedRigidbody.AddForce(10 * GorillaLocomotion.Player.Instance.scale * -GorillaTagger.Instance.offlineVRRig.transform.Find("rig/body/shoulder.R/upper_arm.R/forearm.R/hand.R").right, ForceMode.Acceleration);
                 GorillaTagger.Instance.StartVibration(false, GorillaTagger.Instance.tapHapticStrength / 50f * GorillaLocomotion.Player.Instance.bodyCollider.attachedRigidbody.velocity.magnitude, GorillaTagger.Instance.tapHapticDuration);
             }
         }
