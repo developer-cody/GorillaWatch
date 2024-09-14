7b1800cc0714ba3ce39c3894faf5b638305e665a
ArtificialGorillas <rodericgickle@gmail.com>
2024-09-13 18:42:48 -0700

    Update LowGravity.cs

--- Mods/LowGravity.cs
+++ Mods/LowGravity.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using TheGorillaWatch.Models;
@@ -10,9 +10,19 @@ namespace TheGorillaWatch.Mods
     {
         public override string modName => "LowGravity";
 
+        private bool isEnabled = false;
+
         public override void OnUpdate()
         {
-            GorillaLocomotion.Player.Instance.bodyCollider.attachedRigidbody.AddForce(Vector3.up * (Time.deltaTime * (6.66f / Time.deltaTime)), ForceMode.Acceleration);
+            if (isEnabled)
+            {
+                GorillaLocomotion.Player.Instance.bodyCollider.attachedRigidbody.AddForce(Vector3.up * (Time.deltaTime * (6.66f / Time.deltaTime)), ForceMode.Acceleration);
+            }
+        }
+
+        public void ToggleLowGravity()
+        {
+            isEnabled = !isEnabled;
         }
     }
 }
