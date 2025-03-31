b04647d7d894f1704eb4f9b8a1ca28be87bb1ed0
Cody <rodericgickle@gmail.com>
2025-03-31 15:14:42 -0700

    Update WallRunPatch.cs

--- Patches/WallRunPatch.cs
+++ Patches/WallRunPatch.cs
@@ -8,11 +8,17 @@ namespace TheGorillaWatch.Patches
     [HarmonyPatch(typeof(GTPlayer), "GetSlidePercentage")]
     public class WallRunPatch
     {
+        static Vector3 ogGravity = Physics.gravity;
+        
         static void Postfix(RaycastHit raycastHit)
         {
             if (WallRun.MonkeWallWalkEnabled)
             {
                 Physics.gravity = raycastHit.normal * -9.81f;
+            }
+            else 
+            {
+                Physics.gravity = ogGravity;
             }
         }
     }
