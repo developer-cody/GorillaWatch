debb4bad96f890b19eb962495862d2c1aaacbd03
Cody <rodericgickle@gmail.com>
2025-07-03 17:38:16 -0700

    sdf

--- GorillaWatch/Behaviors/MainComponents/StartUpSound.cs
+++ GorillaWatch/Behaviors/MainComponents/StartUpSound.cs
@@ -1,31 +1,10 @@
-﻿using System.Collections;
-using UnityEngine;
-using UnityEngine.Networking;
+﻿using UnityEngine;
 
 namespace TheGorillaWatch.Behaviors.MainComponents
 {
     internal class StartUpSound : MonoBehaviour
     {
-        private void Start() => GorillaTagger.OnPlayerSpawned(() => StartCoroutine(PlaySound()));
-
-        private IEnumerator PlaySound()
-        {
-            var audioSource = GetComponent<AudioSource>() ?? gameObject.AddComponent<AudioSource>();
-
-            using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip("https://github.com/developer-cody/GorillaWatch/raw/refs/heads/main/Assets/GorillaWatch.wav", AudioType.WAV))
-            {
-                yield return www.SendWebRequest();
-
-                if (www.result != UnityWebRequest.Result.Success)
-                {
-                    Debug.LogError($"Failed to load audio clip: {www.error}");
-                }
-                else
-                {
-                    AudioClip clip = DownloadHandlerAudioClip.GetContent(www);
-                    audioSource.PlayOneShot(clip, 5);
-                }
-            }
-        }
+        private StartUpSound() => GorillaTagger.OnPlayerSpawned(() =>
+            StartCoroutine(Utils.Instance.PlaySoundFromURL("https://github.com/developer-cody/GorillaWatch/raw/refs/heads/main/Assets/GorillaWatch.wav", 5f, AudioType.WAV)));
     }
 }
---
+++ GorillaWatch/Behaviors/Utils.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace TheGorillaWatch.Behaviors
+{
+    internal class Utils : MonoBehaviour
+    {
+        public static Utils Instance;
+
+        private void Start() => Instance = this;
+
+        public IEnumerator PlaySoundFromURL(string URL, float Volume, AudioType type)
+        {
+            var audioSource = GetComponent<AudioSource>() ?? gameObject.AddComponent<AudioSource>();
+
+            using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(URL, type))
+            {
+                yield return www.SendWebRequest();
+
+                if (www.result != UnityWebRequest.Result.Success)
+                {
+                    Debug.LogError($"Failed to load audio clip: {www.error}");
+                }
+                else
+                {
+                    AudioClip clip = DownloadHandlerAudioClip.GetContent(www);
+                    audioSource.PlayOneShot(clip, Volume);
+                }
+            }
+        }
+
+        public static AssetBundle LoadAssetBundleFromURL(string url)
+        {
+            try
+            {
+                using UnityWebRequest unityWebRequest = UnityWebRequestAssetBundle.GetAssetBundle(url);
+                unityWebRequest.SendWebRequest();
+                while (!unityWebRequest.isDone)
+                    Thread.Sleep(10);
+
+                if (unityWebRequest.result != UnityWebRequest.Result.Success)
+                {
+                    Debug.LogError($"Failed to download asset bundle: {unityWebRequest.error}");
+                    return null;
+                }
+
+                return DownloadHandlerAssetBundle.GetContent(unityWebRequest);
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError("Error loading asset bundle from URL: " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
